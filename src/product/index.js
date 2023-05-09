465282a20d992a9c592ae76ccb57290873e1a3e8
csanlucas <csanlucas@live.com>
2023-05-09 11:51:27 -0500

    [CGS-2]: Styling screens (#2)
    * Project setup
    Header, Product components
    Setup navigation on app
    
    * Styling screens
    Handle navigations
    
    * Handle loading and product not found
    
    ---------
    
    Co-authored-by: Cesar San Lucas <csanlucas@live.con>

--- src/product/index.js
+++ src/product/index.js
@@ -1,34 +1,66 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import { IoIosArrowBack } from "react-icons/io";
 
 import Header from '../common/header'
 import Detail from './detail'
 import {API_URL, API_ENDPOINT} from '../utils/api'
+import './Product.css'
 
 const ProductInfo = (props) => {
+    const [isLoading, setIsLoading] = useState(false)
     const [productdetail, setProductDetail] = useState()
     const {sku} = useParams()
+    const navigate = useNavigate()
     useEffect(() => {
         requestProductDetail()
     }, [])
     async function requestProductDetail() {
+        setIsLoading(true)
         try{
             const response = await fetch(`${API_URL}${API_ENDPOINT.PRODUCT_DETAIL_SKU.replace('{value}', sku)}`)
             const json = await response.json()
-            setProductDetail(json['results'][0])
-        } catch {}
+            if (json['results'].length > 0) {
+                setProductDetail(json['results'][0])
+            } else {
+                setProductDetail(undefined)
+            }
+        } catch (error) {}
+        setIsLoading(false)
+    }
+
+    const onClickBack = () => {
+        navigate('/')
+    }
+
+    const getProductContent = () => {
+        if (isLoading) {
+            return (<p>Loading... </p>)
+        } else if (productdetail) {
+            return (
+                <Detail
+                    images={productdetail.images || ''}
+                    name={productdetail.name || ''}
+                    description={productdetail.short_description || ''}
+                    price={Number(productdetail.regular_price).toFixed(2).toLocaleString() || ''}
+                />
+            )
+        } else {
+            return <h2>Producto no encontrado</h2>
+        }
     }
 
     return (
         <div>
             <Header/>
-            {productdetail && (
-                <Detail
-                    name={productdetail.name || ''}
-                    description={productdetail.short_description || ''}
-                    price={productdetail.regular_price || ''}
-                />
-            )}
+            <div className="Product-screen">
+                <div className="Product-back-btn-wrapper">
+                    <button className="Product-back-btn" onClick={onClickBack}>
+                        <h3><IoIosArrowBack/> Regresar</h3>
+                    </button>
+                </div>
+                {getProductContent()}
+            </div>
         </div>
     )
 }
